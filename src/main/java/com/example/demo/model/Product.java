e9916ee66d1c28ea47fc47bcbefae95ca9a8258c
bala-amypo <bala@amypo.in>
2025-12-18 05:03:35 +0000

    User commit

--- src/main/java/com/example/demo/model/Product.java
+++ src/main/java/com/example/demo/model/Product.java
@@ -1,1 +1,15 @@
-package com.example.demo.
+package com.example.demo.entity;
+import jakarta.persistence.*;
+import jakatra.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
+@Entity
+@Table(
+    name="products",
+    uniqueConstraints={
+        @UniqueConstraint(colmunNmaes="sku")
+    }
+)
+public class Product{
+    @Id
+    GeneratedValue(strategy)
+}
