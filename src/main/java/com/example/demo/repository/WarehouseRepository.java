7f73e7e3b96f2d56d7ecc5610315708ae63560bc
bala-amypo <bala@amypo.in>
2025-12-19 08:46:13 +0000

    User commit

--- src/main/java/com/example/demo/repository/WarehouseRepository.java
+++ src/main/java/com/example/demo/repository/WarehouseRepository.java
@@ -1,7 +1,7 @@
 package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo..Warehouse;
+import com.example.demo.model.Warehouse;
 
 public interface WarehouseRepository extends JpaRepository<Warehouse, Long> {
 }
