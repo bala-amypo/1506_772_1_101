91a4c2255e215ea60d25b5f0ce938f07f7c71933
bala-amypo <bala@amypo.in>
2025-12-19 09:28:11 +0000

    User commit

---
+++ src/main/java/com/example/demo/controller/StockRecordController.java
@@ -0,0 +1,30 @@
+package com.example.demo.controller;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import com.example.demo.entity.Warehouse;
+import com.example.demo.service.WarehouseService;
+
+@RestController
+@RequestMapping("/api/warehouses")
+public class WarehouseController {
+
+    @Autowired
+    private WarehouseService warehouseService;
+
+    @PostMapping("/create")
+    public Warehouse createWarehouse(@RequestBody Warehouse warehouse) {
+        return warehouseService.createWarehouse(warehouse);
+    }
+
+    @GetMapping("/all")
+    public List<Warehouse> getAllWarehouses() {
+        return warehouseService.getAllWarehouses();
+    }
+
+    @GetMapping("/{id}")
+    public Warehouse getWarehouse(@PathVariable Long id) {
+        return warehouseService.getWarehouse(id);
+    }
+}
