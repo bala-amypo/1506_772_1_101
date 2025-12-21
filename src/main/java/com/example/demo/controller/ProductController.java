51108459c0d04b03eef0df3cc3e3b25d939f615e
bala-amypo <bala@amypo.in>
2025-12-21 19:57:41 +0000

    User commit

--- src/main/java/com/example/demo/controller/ProductController.java
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -1,40 +1,39 @@
 package com.example.demo.controller;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.example.demo.model.Product;
 import com.example.demo.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/products")
 public class ProductController {
-
+    
+    private final ProductService productService;
+    
     @Autowired
-    private ProductService productService;
-
-    // POST - create product
-    @PostMapping("/create")
-    public Product createProduct(@RequestBody Product product) {
-        return productService.createProduct(product);
+    public ProductController(ProductService productService) {
+        this.productService = productService;
     }
-
-    // GET - list all products
-    @GetMapping("/all")
-    public List<Product> getAllProducts() {
-        return productService.getAllProducts();
+    
+    @PostMapping
+    public ResponseEntity<Product> createProduct(@RequestBody Product product) {
+        Product createdProduct = productService.createProduct(product);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdProduct);
     }
-
-    // GET - get product by id
+    
+    @GetMapping
+    public ResponseEntity<List<Product>> getAllProducts() {
+        List<Product> products = productService.getAllProducts();
+        return ResponseEntity.ok(products);
+    }
+    
     @GetMapping("/{id}")
-    public Product getProduct(@PathVariable Long id) {
-        return productService.getProduct(id);
+    public ResponseEntity<Product> getProduct(@PathVariable Long id) {
+        Product product = productService.getProduct(id);
+        return ResponseEntity.ok(product);
     }
 }
--- src/main/java/com/example/demo/controller/WarehouseController.java
+++ src/main/java/com/example/demo/controller/WarehouseController.java
@@ -1,40 +1,39 @@
 package com.example.demo.controller;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.example.demo.model.Warehouse;
 import com.example.demo.service.WarehouseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/warehouses")
 public class WarehouseController {
-
+    
+    private final WarehouseService warehouseService;
+    
     @Autowired
-    private WarehouseService warehouseService;
-
-    // POST - create warehouse
-    @PostMapping("/create")
-    public Warehouse createWarehouse(@RequestBody Warehouse warehouse) {
-        return warehouseService.createWarehouse(warehouse);
+    public WarehouseController(WarehouseService warehouseService) {
+        this.warehouseService = warehouseService;
     }
-
-    // GET - list all warehouses
-    @GetMapping("/all")
-    public List<Warehouse> getAllWarehouses() {
-        return warehouseService.getAllWarehouses();
+    
+    @PostMapping
+    public ResponseEntity<Warehouse> createWarehouse(@RequestBody Warehouse warehouse) {
+        Warehouse createdWarehouse = warehouseService.createWarehouse(warehouse);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdWarehouse);
     }
-
-    // GET - get warehouse by id
+    
+    @GetMapping
+    public ResponseEntity<List<Warehouse>> getAllWarehouses() {
+        List<Warehouse> warehouses = warehouseService.getAllWarehouses();
+        return ResponseEntity.ok(warehouses);
+    }
+    
     @GetMapping("/{id}")
-    public Warehouse getWarehouse(@PathVariable Long id) {
-        return warehouseService.getWarehouse(id);
+    public ResponseEntity<Warehouse> getWarehouse(@PathVariable Long id) {
+        Warehouse warehouse = warehouseService.getWarehouse(id);
+        return ResponseEntity.ok(warehouse);
     }
 }
