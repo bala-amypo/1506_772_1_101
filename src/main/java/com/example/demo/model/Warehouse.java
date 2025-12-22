7866505cc9ca1e8a9b5e51a6e211b80494e009e4
bala-amypo <bala@amypo.in>
2025-12-22 00:24:32 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/Warehouse.java
@@ -0,0 +1,38 @@
+package com.example.demo.model;
+
+import java.time.LocalDateTime;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "warehouses")
+public class Warehouse {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String warehouseName;
+    private String location;
+    private LocalDateTime createdAt;
+
+    public Warehouse() {}
+
+    public Warehouse(Long id, String warehouseName, String location, LocalDateTime createdAt) {
+        this.id = id;
+        this.warehouseName = warehouseName;
+        this.location = location;
+        this.createdAt = createdAt;
+    }
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getWarehouseName() { return warehouseName; }
+    public void setWarehouseName(String warehouseName) { this.warehouseName = warehouseName; }
+
+    public String getLocation() { return location; }
+    public void setLocation(String location) { this.location = location; }
+
+    public LocalDateTime getCreatedAt() { return createdAt; }
+    public void setCreatedAt(LocalDateTime createdAt) { this.createdAt = createdAt; }
+}
