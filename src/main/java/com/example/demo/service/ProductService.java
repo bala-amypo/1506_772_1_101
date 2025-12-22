4a69be1018fd7146b70d9dcff62c01f70b9b77d0
bala-amypo <bala@amypo.in>
2025-12-22 01:06:33 +0000

    User commit

--- src/main/java/com/example/demo/service/ProductService.java
+++ src/main/java/com/example/demo/service/ProductService.java
@@ -2,11 +2,13 @@ package com.example.demo.service;
 
 import java.util.List;
 
-import com.example.demo.model.PredictionRule;
+import com.example.demo.model.Product;
 
-public interface PredictionService {
+public interface ProductService {
 
-    PredictionRule createRule(PredictionRule rule);
+    Product createProduct(Product product);
 
-    List<PredictionRule> getAllRules();
+    Product getProduct(Long id);
+
+    List<Product> getAllProducts();
 }
