537a5a41e59311b0da5392fb5307b0e0f3ee2a33
bala-amypo <bala@amypo.in>
2025-12-26 09:35:32 +0000

    User commit

--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -1,3 +1,10 @@
+
+package com.example.demo.repository;
+
+import com.example.demo.model.UserRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 public interface PredictionRuleRepository extends JpaRepository<PredictionRule, Long> {
     Optional<PredictionRule> findByRuleName(String ruleName);
 }
