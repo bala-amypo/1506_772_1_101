ec5056db0f7e85f9fc605fac7b9664a044e2309b
bala-amypo <bala@amypo.in>
2025-12-26 16:54:59 +0000

    User commit

--- src/main/java/com/example/demo/dto/AuthRequest.java
+++ src/main/java/com/example/demo/dto/AuthRequest.java
@@ -1,4 +1,4 @@
-package com.example.demo.dto;
+/*package com.example.demo.dto;
 
 import lombok.*;
 
@@ -8,5 +8,25 @@ import lombok.*;
 @AllArgsConstructor
 public class AuthRequest {
     private String email;
+    private String password;
+}
+*/
+package com.example.demo.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AuthRequest {
+    
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
+    private String email;
+    
+    @NotBlank(message = "Password is required")
     private String password;
 }
