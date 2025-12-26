f2740084aa98233236bcf8c93f243306e7381ccc
bala-amypo <bala@amypo.in>
2025-12-26 05:50:19 +0000

    User commit

--- src/main/java/com/example/demo/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.model.*;
-import java.util.*;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-    Optional<Product> findBySku(String sku);
-}
--- src/main/java/com/example/demo/repository/WarehouseRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import java.util.Optional;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.model.Warehouse;
-public interface WarehouseRepository extends JpaRepository<Warehouse, Long> {}
