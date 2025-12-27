d4dabe261fcd48343fcaa88905506e75cdf62b15
bala-amypo <bala@amypo.in>
2025-12-27 19:25:04 +0000

    User commit

--- src/main/java/com/example/demo/Service/WarehouseService.java
+++ src/main/java/com/example/demo/Service/WarehouseService.java
@@ -1,21 +1,14 @@
 package com.example.demo.service;
 
 import com.example.demo.model.Warehouse;
-import com.example.demo.repository.WarehouseRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
 
-@Service
-@RequiredArgsConstructor
-public class WarehouseService {
-    private final WarehouseRepository warehouseRepository;
-    
-    public Warehouse createWarehouse(Warehouse warehouse) {
-        return warehouseRepository.save(warehouse);
-    }
-    
-    public Warehouse getWarehouse(Long id) {
-        return warehouseRepository.findById(id)
-                .orElseThrow(() -> new ResourceNotFoundException("Warehouse not found"));
-    }
+import java.util.List;
+
+public interface WarehouseService {
+
+    Warehouse createWarehouse(Warehouse warehouse);
+
+    Warehouse getWarehouse(Long id);
+
+    List<Warehouse> getAllWarehouses();
 }
