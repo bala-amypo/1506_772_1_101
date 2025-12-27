c3af8932a262c048274e3913f301df15a2a6a2d2
bala-amypo <bala@amypo.in>
2025-12-27 05:40:13 +0000

    User commit

---
+++ src/main/java/com/example/demo/dto/AuthRequest.java
@@ -0,0 +1,18 @@
+package com.example.demo.dto;
+
+import lombok.Data;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Data
+public class AuthRequest {
+    
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email should be valid")
+    private String email;
+    
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Password must be at least 6 characters")
+    private String password;
+}
